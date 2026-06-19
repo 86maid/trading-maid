41da6e8656d45d3eb4dd44bfc5ce50d39394af7d
86maid <134382214+86maid@users.noreply.github.com>
2026-06-19 14:47:17 +0800

    负资金费率做多

---
+++ examples/funding_rate.rs
@@ -0,0 +1,89 @@
+use trading_maid::prelude::*;
+
+// 资金费率为负的时候做多
+async fn my_strategy(cx: &Context<'_>) -> anyhow::Result<()> {
+    let funding_rate = &cx["funding_rate"];
+
+    if funding_rate != &[] {
+        if cx.get_position("BTCUSDT").await?.is_none()
+            && funding_rate < 0
+            && funding_rate[1] < 0
+            && funding_rate[2] < 0
+            && funding_rate[3] < 0
+            && funding_rate[4] < 0
+        {
+            println!(
+                "place order: {}, funding_rate: {}",
+                t2s(cx.time),
+                funding_rate
+            );
+
+            let take_profit_price = cx.open + 1000;
+            let stop_price = cx.open - 1000;
+
+            cx.cancel_all_order("BTCUSDT").await?;
+
+            _ = cx
+                .buy_tp_sl("BTCUSDT", take_profit_price, stop_price, 0.01)
+                .await?;
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let path = get_or_download("BTCUSDT/1m", 12).await.unwrap();
+
+    let data_source_1m = DataSource::from_file_metadata(
+        path,
+        Metadata {
+            symbol: "BTCUSDT".to_string(),
+            level: Level::Minute1,
+            min_size: "0.01".parse().unwrap(),
+            min_notional: "0".parse().unwrap(),
+            tick_size: "0.1".parse().unwrap(),
+            maker_fee: "0.0002".parse().unwrap(),
+            taker_fee: "0.0005".parse().unwrap(),
+            maintenance: "0.004".parse().unwrap(),
+        },
+    )
+    .unwrap();
+
+    let exchange = LocalExchange::new(data_source_1m.clone())
+        .cash(10000)
+        .leverage(10)
+        .slippage(0);
+
+    let mut engine = Engine::new(exchange.clone(), my_strategy);
+
+    engine.add_series(
+        "BTCUSDT",
+        "funding_rate",
+        get_or_download_funding_rate_to_series("BTCUSDT", 12, Level::Hour8)
+            .await
+            .unwrap(),
+    );
+
+    if let Err(v) = engine.run("BTCUSDT", Level::Hour8).await {
+        println!("error: {:#?}", v);
+    }
+
+    let history_position = exchange.get_history_position_list("BTCUSDT").await.unwrap();
+    let history_order = exchange.get_history_order_list("BTCUSDT").await.unwrap();
+    let summary = summarize(&history_position);
+
+    println!("history summary: {:#?}", summary);
+
+    let data_source_4h = data_source_1m.resample(Level::Hour4).unwrap();
+    let data_source_1h = data_source_1m.resample(Level::Hour1).unwrap();
+
+    open_in_server(
+        [data_source_4h, data_source_1h, data_source_1m],
+        history_position,
+        history_order,
+    )
+    .await
+    .unwrap();
+}
